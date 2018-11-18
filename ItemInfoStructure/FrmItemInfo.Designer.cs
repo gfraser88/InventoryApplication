1b9397121f40ca70f7298c1f7fb3b056178f0f0d
gfraser4 <45044460+gfraser4@users.noreply.github.com>
2018-11-18 16:53:01 -0500

    Initial commit

---
+++ ItemInfoStructure/FrmItemInfo.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ItemInfoStructure
+{
+    partial class FrmItemInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Item &Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Item &Price:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Item &Description:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(104, 9);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(104, 44);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 3;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(104, 79);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(100, 20);
+            this.txtDescription.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(15, 124);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 44);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "&Add Item";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnDisplay.Location = new System.Drawing.Point(129, 124);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 44);
+            this.btnDisplay.TabIndex = 7;
+            this.btnDisplay.Text = "&Show Items";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // FrmItemInfo
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnDisplay;
+            this.ClientSize = new System.Drawing.Size(224, 185);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmItemInfo";
+            this.Text = "Item Information";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDisplay;
+    }
+}
+
---
+++ ItemInfoStructure/FrmItemInfo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//Graeme Fraser
+namespace ItemInfoStructure
+{
+    public partial class FrmItemInfo : Form
+    {
+        public FrmItemInfo()
+        {
+            InitializeComponent();
+        }
+
+        List<Item> items = new List<Item>();
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Item item = new Item();
+
+                item.ItemNumber = int.Parse(txtNumber.Text);
+                item.ItemPrice = double.Parse(txtPrice.Text);
+                item.ItemDescription = txtDescription.Text;
+
+                items.Add(item);
+
+                txtNumber.Clear();
+                txtDescription.Clear();
+                txtPrice.Clear();
+                txtNumber.Focus();
+            }
+
+            catch(FormatException)
+            {
+                MessageBox.Show("Invalid Input", "Error");
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            StringBuilder display = new StringBuilder();
+
+            foreach(Item i in items)
+            {
+                display.Append("Item Number: " + i.ItemNumber + "\nItem Price: " + i.ItemPrice + "\nItem Description: " + i.ItemDescription + "\n\n");
+            }
+
+            MessageBox.Show(display.ToString(), Item.StoreName);
+        }
+    }
+
+    public struct Item
+    {
+        public const string StoreName = "SportChek";
+        private int number;
+        private double price;
+        private string description;
+
+        public int ItemNumber
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        public double ItemPrice
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public string ItemDescription
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+    }
+}
